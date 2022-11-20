7dc0bb35861e5bc51cc74d614cb9ee504f874e63
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2022-11-20 18:42:59 +0400

    feat: add guess the number number generation and game logic

--- guess/src/main.rs
+++ guess/src/main.rs
@@ -1,15 +1,35 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Welcome to the 'Guess a Number show' 🧙");
 
-    println!("Your turn. Guess a number 🔮");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    let mut guess = String::new();
+    loop {
+        println!("Your turn. Guess a number 🔮");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Oops, I wasn't able to read your guess 😱");
+        let mut guess = String::new();
 
-    println!("You guessed: {guess}");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Oops, I wasn't able to read your guess 😱");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed 👉 {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("You need to be more ambitious 👎"),
+            Ordering::Greater => println!("A little more humble next time 😵‍💫"),
+            Ordering::Equal => {
+                println!("Congratulations! Where should we wire the mulah? 💰");
+                break;
+            }
+        }
+    }
 }
