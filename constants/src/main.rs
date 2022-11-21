b182b0f9e34006b795d85b04c1f24293e5ea1d1c
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2022-11-21 21:38:13 +0400

    feat: add constant example

---
+++ constants/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    const WORKING_HOURS_IN_A_MONTH: u32 = 8 * 5 * 4;
+
+    println!("How many working hours are in a month? 🙄");
+    println!("And the answer is...{WORKING_HOURS_IN_A_MONTH} 😱");
+}
