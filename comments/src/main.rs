8e246fd5dce752b63139deb5c87013d9f06817e5
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2022-11-24 12:36:39 +0400

    feat: add comments

---
+++ comments/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Welcome to the art of commenting 💭");
+
+    // 42 represents the meaning of life
+    let x = 42;
+
+    println!("Can you explain me what this number is about? {} 🤯", x);
+}
