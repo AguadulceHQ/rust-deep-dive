7876f1067c066ef1178070e4f40269a683a76068
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2022-11-30 22:19:09 +0400

    feat: add match enum bind values pattern

--- match-control-flow/src/main.rs
+++ match-control-flow/src/main.rs
@@ -1,22 +1,31 @@
 enum CryptoCoin {
     Bitcoin,
     Altcoin,
-    Shitcoin,
+    Shitcoin(MarketCapitalization),
+}
+
+enum MarketCapitalization {
+    High,
+    Mid,
+    Low,
+    Micro,
 }
 
 fn main() {
     let my_crypto = CryptoCoin::Bitcoin;
     let my_altcoin = CryptoCoin::Altcoin;
+    let my_shitcoin = CryptoCoin::Shitcoin(MarketCapitalization::High);
 
     println!("Match allows to control the flow according to the value of the variable. I have BTC and it's rating is {} 💰", rating(my_crypto));
     println!("We can also use a catch-all pattern for those values that we don't want to handle explicitly. I have ETH and it's rating is {} 🙀", maximalist_rating(my_altcoin));
+    println!("Match is great to use with enum and bind to their values so that we can run some code. For example my shitcoin may not be that bad 👉 {} 🙏", shit_rating(my_shitcoin));
 }
 
 fn rating(coin: CryptoCoin) -> u8 {
     match coin {
         CryptoCoin::Bitcoin => 100,
         CryptoCoin::Altcoin => 10,
-        CryptoCoin::Shitcoin => 0,
+        CryptoCoin::Shitcoin(_) => 0,
     }
 }
 
@@ -24,5 +33,19 @@ fn maximalist_rating(coin: CryptoCoin) -> u8 {
     match coin {
         CryptoCoin::Bitcoin => 100,
         _ => 0,
+    }
+}
+
+fn shit_rating(coin: CryptoCoin) -> u8 {
+    match coin {
+        CryptoCoin::Bitcoin => 100,
+        CryptoCoin::Altcoin => 10,
+        CryptoCoin::Shitcoin(market_cap) => {
+            if matches!(market_cap, MarketCapitalization::High) {
+                println!("At least sounds like a high market cap 💩");
+                return 10;
+            }
+            0
+        }
     }
 }
