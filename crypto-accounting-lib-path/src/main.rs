f003b6e26f494c8a007fdedbf0a49982c121d793
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2022-12-02 23:49:44 +0400

    feat: add usage of external package and nested path use case

--- crypto-accounting-lib-path/src/main.rs
+++ crypto-accounting-lib-path/src/main.rs
@@ -1,9 +1,12 @@
-use crypto_accounting_lib_path::balance_sheet;
-use crypto_accounting_lib_path::balance_sheet::transaction::Transaction;
+// using a nested path here to condense the use statement
+// we bring in scope Transaction with a full path
+use crypto_accounting_lib_path::balance_sheet::{self, transaction::Transaction};
+use rand::Rng;
+
 fn main() {
     let first_transaction = Transaction {
         asset: String::from("BTC"),
-        amount: 42.0,
+        amount: rand::thread_rng().gen_range(1.0..=100.0),
         invoice: 1,
         recoinciled: false,
         description: String::from("Payment to Aguadulce to make it shine! 🤑"),
