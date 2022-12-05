8cf15b90af0cf80712f1468aa279159e8beed809
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2022-12-05 22:20:29 +0400

    feat: add match error handling

--- error-handling/src/main.rs
+++ error-handling/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs::File;
+use std::io::ErrorKind;
 
 fn main() {
     // a crash caused voluntarily
@@ -7,4 +8,30 @@ fn main() {
     // an unwanted crash
     // let scores = vec![10, 100, 1000];
     // scores[42];
+
+    let onboarding_file = File::open("onboarding.txt");
+
+    let onboarding = match onboarding_file {
+        Ok(file) => file,
+        Err(error) => panic!("Cannot open the onboarding file: {:?}", error),
+    };
+
+    println!("{:?}", onboarding);
+
+    let project_file = File::open("project.txt");
+
+    let project = match project_file {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            // io:Error is a struct with method kind
+            ErrorKind::NotFound => match File::create("project.txt") {
+                // kind returns io::ErrorKind value. Create also could fail so we need another match
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error);
+            }
+        },
+    };
 }
