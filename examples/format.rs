d57d03f52273135bd1044f9f3c95f96a3b4c77fc
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2022-12-06 21:38:37 +0400

    feat: implement Display trait for unspecified format string argument type

---
+++ examples/format.rs
@@ -0,0 +1,85 @@
+// formatting is specified via a format string
+// the same variable can be formatted differently depending on the argument type used in the format string
+// the formatting functionality is impelmented via traits and there is a trait for each argument type
+// the most common trait is Display which handles when the argument type is unspecified {}
+use std::fmt::{self, Display, Formatter};
+
+struct City {
+    name: &'static str,
+    // Latitude
+    lat: f32,
+    // Longitude
+    lon: f32,
+}
+
+impl Display for City {
+    // 'f' is a buffer and the method must write the formatted string into it
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+        let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
+
+        // write! the formatted string into a buffer (the first argument)
+        write!(
+            f,
+            "{}: {:.3}°{} {:.3}°{}",
+            self.name,
+            self.lat.abs(),
+            lat_c,
+            self.lon.abs(),
+            lon_c
+        )
+    }
+}
+
+#[derive(Debug)]
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+fn main() {
+    let cities = vec![
+        City {
+            name: "Dublin",
+            lat: 53.347778,
+            lon: -6.259722,
+        },
+        City {
+            name: "Oslo",
+            lat: 59.95,
+            lon: 10.75,
+        },
+        City {
+            name: "Vancouver",
+            lat: 49.25,
+            lon: -123.1,
+        },
+    ];
+
+    for city in cities.iter() {
+        println!("{}", *city);
+    }
+
+    for color in [
+        Color {
+            red: 128,
+            green: 255,
+            blue: 90,
+        },
+        Color {
+            red: 0,
+            green: 3,
+            blue: 254,
+        },
+        Color {
+            red: 0,
+            green: 0,
+            blue: 0,
+        },
+    ]
+    .iter()
+    {
+        println!("{:?}", *color);
+    }
+}
