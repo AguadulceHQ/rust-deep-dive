d17a93e12a955fe515d8268cc6c40bcd23607276
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2022-12-10 21:39:13 +0400

    feat: add generic type to a struct

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -1,3 +1,10 @@
+// we are having two generic types to be able to manage integer and decimals as ratings
+#[derive(Debug)]
+struct Feedback<CR, DR> {
+    communication_rating: CR,
+    deliverables_rating: DR,
+}
+
 fn main() {
     println!("Generic types allow to avoid code duplication 🔝");
 
@@ -8,6 +15,13 @@ fn main() {
     let decimals = vec![3.0, 2.0, 42.0, 3.0];
 
     println!("The max of the decimals is {}", maximum(&decimals));
+
+    let feedback = Feedback {
+        communication_rating: 4,
+        deliverables_rating: 5.0,
+    };
+
+    println!("Feedback release from client is {:?}", feedback);
 }
 
 // we need PartialOrd trait so that order is supported for this generic type
