70aeca7c7abde6c020feab94698df85b1d9bb9d0
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2022-12-12 22:30:09 +0400

    feat: add lifetime example with only one used parameter

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -5,6 +5,10 @@ fn main() {
     let result = projects_order(project_one.as_str(), project_two.as_str());
 
     println!("The first project in our order should be {}", result);
+
+    let priority = projects_reset_priority(project_one.as_str(), project_two.as_str());
+
+    println!("The priority got changed now we will focus on {}", priority);
 }
 
 fn projects_order<'a>(first_project: &'a str, second_project: &'a str) -> &'a str {
@@ -13,4 +17,10 @@ fn projects_order<'a>(first_project: &'a str, second_project: &'a str) -> &'a st
     } else {
         second_project
     }
+}
+
+// dummy example but here we don't care of the lifetime of second_project anyway we are not returning it
+// so it can safely go out of scope
+fn projects_reset_priority<'a>(first_project: &'a str, second_project: &str) -> &'a str {
+    first_project
 }
