a70c40b8c1482883d4f5c7cd71fa800ece99f39d
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2022-12-17 19:02:27 +0400

    feat: add basic CLI program

---
+++ cli-program/src/main.rs
@@ -0,0 +1,22 @@
+// bring into scope so that we can use args fn
+// we bring parent module in as the fn is 2 level nested at least it’s clear it comes from a module
+// otherwise you may mistake args for your own fn
+
+use std::env;
+use std::fs;
+
+fn main() {
+    // we specify that we want a vector of Strings as collect can return other types of collections too
+    // Rust generally infers the type but with this method we need to specify as it can return multiple
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
+
+    // returns std::io::Result<String> that contain file’s contents
+    let file_contents = fs::read_to_string(file_path).expect("Not able to read the file");
+
+    println!("Text is\n\n {}", file_contents);
+}
