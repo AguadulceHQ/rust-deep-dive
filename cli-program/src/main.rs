29b9defbd27c462066ad4614877c9ac181aeea88
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2022-12-17 19:10:10 +0400

    refactor(config): extract struct of configuration instead of anonymous tuple

--- cli-program/src/main.rs
+++ cli-program/src/main.rs
@@ -5,26 +5,36 @@
 use std::env;
 use std::fs;
 
+struct Config {
+    query: String,
+    file_path: String,
+}
+
 fn main() {
     // we specify that we want a vector of Strings as collect can return other types of collections too
     // Rust generally infers the type but with this method we need to specify as it can return multiple
     let args: Vec<String> = env::args().collect();
 
-    let (query, file_path) = parse_config(&args);
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
 
     // returns std::io::Result<String> that contain file’s contents
-    let file_contents = fs::read_to_string(file_path).expect("Not able to read the file");
+    let file_contents = fs::read_to_string(config.file_path).expect("Not able to read the file");
 
     println!("Text is\n\n {}", file_contents);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
+// this now returns a Config value
+fn parse_config(args: &[String]) -> Config {
+    // owned strings go into the new defined struct
+    let query = args[1].clone();
 
-    // we return references to arguments to which main has ownership
-    (query, file_path)
+    // generally clone is avoided in this case the tradeoff is worth
+    let file_path = args[2].clone();
+
+    // clone cannot take ownership but only borrow from main
+    // we need to clone those field to have ownership
+    Config { query, file_path }
 }
