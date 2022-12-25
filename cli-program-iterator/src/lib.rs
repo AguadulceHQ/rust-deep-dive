f31e8ed72a3e8aac435a6108e18a903d282a2177
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2022-12-25 18:16:48 +0400

    refactor(iterators): add iterators in place of loops that leverage intermediate vec for state

--- cli-program-iterator/src/lib.rs
+++ cli-program-iterator/src/lib.rs
@@ -63,28 +63,20 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    // by using an iterator and a closure we avoid using a temporary vector that handles state
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_non_sensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
 
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
