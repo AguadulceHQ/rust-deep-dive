f9d3d9ecb31d208d5de35b9185303848fbabd8fb
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2022-12-31 21:24:57 +0400

    feat: add custom box implementation with deref trait

--- deref/src/main.rs
+++ deref/src/main.rs
@@ -1,3 +1,27 @@
+use std::ops::Deref;
+
+struct TaskBox<T>(T); // we want to hold any type so we use generic parameter
+
+impl<T> TaskBox<T> {
+    fn new(x: T) -> TaskBox<T> {
+        TaskBox(x)
+    }
+}
+
+// we are implementing the Deref trait so that the compiler knows how to do it
+impl<T> Deref for TaskBox<T> {
+    type Target = T; // this is an associated type (a way of declaring sort of a generic parameter)
+
+    fn deref(&self) -> &Self::Target {
+        &self.0 // returns a reference to the value we want to access with * operator
+                // .0 accesses the first value in a tupe struct
+    }
+}
+
+fn print_task(task: &str) {
+    println!("Message from the box 📦. The task is: {task}");
+}
+
 fn main() {
     let x = 5;
     let y = &x; // reference
@@ -12,4 +36,10 @@ fn main() {
     println!("z is equal to w in fact 👉 {} = {}", z, *w);
     assert_eq!(5, z);
     assert_eq!(5, *w); // dereference
+
+    println!("Let's create our own Box that implements the deref trait 🧠");
+
+    let task = TaskBox::new(String::from("Upgrade version of Polkadot to latest"));
+    print_task(&task); // reference to a TaskBox<String> value but because we have the Deref trait Rust can turn &TaskBox<String> into &String by calling deref
+                       // deref is then called again because String returns a string slice, so it converts &String to &str to match print_task fn definition
 }
