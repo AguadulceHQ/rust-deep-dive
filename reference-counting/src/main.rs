b81c7682da0969f7cccee27bbb4a3a197dd43a66
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2023-01-01 22:58:50 +0400

    feat: add count references of Rc<T> as variables go out of scope and owners change

--- reference-counting/src/main.rs
+++ reference-counting/src/main.rs
@@ -15,15 +15,34 @@ fn main() {
             Rc::new(Nil),
         )),
     ));
+    println!(
+        "count references to major_task after creating major_task = {}",
+        Rc::strong_count(&major_task)
+    );
+
     let higher_priority_task = Cons(
         String::from("Deploy parachain code"),
         Rc::clone(&major_task),
     ); // we clone by passing a reference to a
-    let highest_priority_task = Cons(String::from("Reserve ParaID"), Rc::clone(&major_task));
-    // we clone by passing a reference to a
-
     println!(
-        "An Rc smart pointer allows to have multiple owners of some data. The task lists is {:?}",
-        highest_priority_task
+        "count references to major_task after creating higher_priority_task = {}",
+        Rc::strong_count(&major_task)
+    );
+    {
+        let highest_priority_task = Cons(String::from("Reserve ParaID"), Rc::clone(&major_task));
+        // we clone by passing a reference to a
+        println!(
+            "count references to major_task after creating highest_priority_task = {}",
+            Rc::strong_count(&major_task)
+        );
+
+        println!(
+            "An Rc smart pointer allows to have multiple owners of some data. The task lists is {:?}",
+            highest_priority_task
+        );
+    }
+    println!(
+        "count references to major_task after highest priority task goes out of scope = {}",
+        Rc::strong_count(&major_task)
     );
 }
