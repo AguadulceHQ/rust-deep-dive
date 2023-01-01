e7cf56217610e2f2e709919f954460dcee921510
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2023-01-01 22:46:21 +0400

    feat: add example of explicit drop call before going out of scope

--- drop/src/main.rs
+++ drop/src/main.rs
@@ -24,4 +24,13 @@ fn main() {
         task_one, task_two
     );
     println!("Tasks have been created. But we are going out of scope after main is done let's see who gets de-allocated first, the compiler will call our implementation of drop before de-allocating the memory.");
+
+    let task_three = TaskPointer {
+        data: String::from("This task will be dropped before main goes out of scope"),
+    };
+
+    println!("Task three has been created, we are in main");
+    // this is defined in std::mem::drop and takes the value that you want to drop
+    drop(task_three);
+    println!("Task three has been dropped although we are still in main, task one and two in fact will get dropped after this");
 }
