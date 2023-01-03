6592e71b9ccbb801b99f1d8fc3fcdd9b4a2e5d05
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2023-01-03 22:24:14 +0400

    feat: add counting of strong_count and weak_count to appreciate memory management

--- tree-weak/src/main.rs
+++ tree-weak/src/main.rs
@@ -22,17 +22,47 @@ fn main() {
     // the parent is None
     println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
 
-    // we crate a new Node (contained in Rc) with leaf as a children and no parent
-    let branch = Rc::new(Node {
-        value: 42,
-        parent: RefCell::new(Weak::new()),
-        children: RefCell::new(vec![Rc::clone(&leaf)]),
-    });
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );
+    {
+        println!("We are getting into an inner scope here to test counting");
+        // we crate a new Node (contained in Rc) with leaf as a children and no parent
+        let branch = Rc::new(Node {
+            value: 42,
+            parent: RefCell::new(Weak::new()),
+            children: RefCell::new(vec![Rc::clone(&leaf)]),
+        });
 
-    // update reference of parent add a Weak<Node> reference
-    // we get RefCell<Weak<Node>> and downgrade gives us Weak<Node>
-    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+        // update reference of parent add a Weak<Node> reference
+        // we get RefCell<Weak<Node>> and downgrade gives us Weak<Node>
+        *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
 
+        // Rc<Node> in branch has sc 1 and wk 1 (leaf.parent points to branch with Weak<Node>)
+        println!(
+            "branch strong = {}, weak = {}",
+            Rc::strong_count(&branch),
+            Rc::weak_count(&branch),
+        );
+
+        // leaf has sc 2 because branch now has a clone of the Rc<Node> of leaf stored in branch.children, wc is 0
+        println!(
+            "leaf strong = {}, weak = {}",
+            Rc::strong_count(&leaf),
+            Rc::weak_count(&leaf),
+        );
+    }
+
+    println!("We are now in the outer scope let's check counts, branch no longer exists as its sc was 0 so it has been dropped");
     // now we can access the parent from the children and thanks to Weak without creating reference cycles
+    // wc is still 1 but because Rc<Node> has sc 0 the Node is dropped
+    // left has sc of 1 because leaf is now the only reference to the Rc<Node>
     println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+    println!(
+        "leaf strong = {}, weak = {}",
+        Rc::strong_count(&leaf),
+        Rc::weak_count(&leaf),
+    );
 }
