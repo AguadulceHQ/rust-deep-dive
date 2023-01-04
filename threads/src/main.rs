a4d5d748c0c2027494c8445b34e7861bafe7ba66
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2023-01-04 21:46:02 +0400

    feat: add handle usage to complete spawned threads before resuming exec on main

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -3,7 +3,6 @@ use std::time::Duration;
 
 fn main() {
     // this method creates a new thread
-
     thread::spawn(|| {
         for i in 1..10 {
             println!("Web Server Request #{} from the spawned thread", i);
@@ -14,6 +13,25 @@ fn main() {
 
     for i in 1..5 {
         println!("Web Server Main Thread Execution #{}", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("Web Server 2 Request #{} from the 2nd spawned thread", i);
+            // allows a different thread to run in the meantime
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    handle.join().unwrap();
+
+    println!(
+        "All spawned threads have finished their execution control back to the main thread to 👉 move ahead with execution"
+    );
+
+    for i in 1..5 {
+        println!("Web Server 2 Main Thread Execution #{}", i);
         thread::sleep(Duration::from_millis(1));
     }
 }
