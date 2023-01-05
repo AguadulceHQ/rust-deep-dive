fc407c8bef2957d08b2feae3134a55be39248f1f
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2023-01-05 22:06:58 +0400

    feat: add receiver to channel for message passing from spawned thread to main

--- threads-message-passing/src/main.rs
+++ threads-message-passing/src/main.rs
@@ -12,9 +12,14 @@ fn main() {
 
     // we move tx into the closure so that the spawned thread owns tx
     thread::spawn(move || {
-        let val = String::from("New Project");
+        let project = String::from("New Project");
         // method that sends a value through the transmitter
         // returns Result<T, E> if the receiver has been dropped it returns an error
-        tx.send(val).unwrap();
+        tx.send(project).unwrap();
     });
+    // recv (receive) blocks the main thread’s execution and wait until a value is sent down the channel
+    // once a value is sent recv will return in Result<T, E>
+    // if the transmitter closes recv will return an error to signal that no more values are coming
+    let new_project = rx.recv().unwrap();
+    println!("New project added to pipeline: {}", new_project);
 }
