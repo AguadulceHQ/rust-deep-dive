45e1ace823dc732386b1b19d2287556feb2f1b26
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2023-01-09 21:28:39 +0400

    feat: refactor the OOP pattern implementation in a Rust idiomatic way

---
+++ oop-pattern-idiomatic/src/lib.rs
@@ -0,0 +1,51 @@
+pub struct Post {
+    // this stays private we don't want to expose it directly
+    content: String,
+}
+
+// we moved the encoding of the state to the type of the struct
+pub struct DraftPost {
+    // this is private
+    content: String,
+}
+
+impl Post {
+    // this now returns a DraftPost instead of a Post object
+    pub fn new() -> DraftPost {
+        DraftPost {
+            content: String::new(),
+        }
+    }
+
+    pub fn content(&self) -> &str {
+        &self.content
+    }
+}
+
+impl DraftPost {
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
+    }
+
+    // take ownership of self (consuming DraftPost instance) and transforming into PendingReviewPost
+    pub fn request_revioew(self) -> PendingReviewPost {
+        PendingReviewPost {
+            content: self.content,
+        }
+
+        // there is no content method so the compiler will not compile the program if we call it
+    }
+}
+
+pub struct PendingReviewPost {
+    content: String,
+}
+
+impl PendingReviewPost {
+    // this is the only way to get content ouf ot a post we need to approve it
+    pub fn approve(self) -> Post {
+        Post {
+            content: self.content,
+        }
+    }
+}
