1aad1fb28d6fb1056d2da185cc9be1e99f8ce86c
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2023-01-12 21:46:14 +0400

    feat: add destructure pattern for structs and nested enums

--- patterns-destructure/src/main.rs
+++ patterns-destructure/src/main.rs
@@ -1,11 +1,22 @@
+struct Feedback {
+    communication: i32,
+    overall: i32,
+}
+
+enum ProjectStatus {
+    Analysis,
+    Quote { time: i32, amount: i32 },
+    Acceptance(bool),
+    Started(TeamAssigned),
+}
+
+enum TeamAssigned {
+    TeamWithManager(i32, String, String),
+    TeamWithNoManager(i32, String),
+}
+
 fn main() {
-    // match deconstructing structs
-
-    struct Feedback {
-        communication: i32,
-        overall: i32,
-    }
-
+    // deconstructing structs
     let f1 = Feedback {
         communication: 3,
         overall: 4,
@@ -25,4 +36,33 @@ fn main() {
     assert_eq!(3, communication);
     assert_eq!(4, o);
     assert_eq!(4, overall);
+
+    // deconstructing enums
+    let new_project = ProjectStatus::Started(TeamAssigned::TeamWithManager(
+        42,
+        String::from("Luca"),
+        String::from("Tibi"),
+    ));
+
+    match new_project {
+        ProjectStatus::Analysis => {
+            // we can just match the literal as ProjectStatus::Analysis value has no variables
+            println!("The Analysis variant has no data to destructure");
+        }
+        ProjectStatus::Quote { time, amount } => {
+            // list the fields with variables and break them apart with shorthand form so that we can use them
+            println!("The project will take {time} time and will cost {amount}");
+        }
+        ProjectStatus::Acceptance(status) => {
+            println!("The project has been accepted: {status}");
+        }
+        ProjectStatus::Started(TeamAssigned::TeamWithManager(project, owner, dev)) => {
+            // the number of variables matches the number of elements in the variant
+            println!("The project with id {project} has started. The responsible is {owner} and the developer is {dev}");
+        }
+        ProjectStatus::Started(TeamAssigned::TeamWithNoManager(project, dev)) => {
+            // the number of variables matches the number of elements in the variant
+            println!("The project with id {project} has started. The developer is {dev}");
+        }
+    }
 }
