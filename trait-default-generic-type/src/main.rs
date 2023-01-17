e43052800aeea06d903b27c3b21b662a023a8ab1
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2023-01-17 21:59:58 +0400

    feat: add operator overloading with overriding of default type parameter

--- trait-default-generic-type/src/main.rs
+++ trait-default-generic-type/src/main.rs
@@ -1,5 +1,5 @@
 use std::ops::Add;
-
+use std::ops::Sub;
 #[derive(Debug, Copy, Clone, PartialEq)]
 
 struct BudgetRange {
@@ -20,9 +20,28 @@ impl Add for BudgetRange {
     }
 }
 
+// overriding the default parameter type for the trait
+struct Discount(i32);
+
+impl Sub<Discount> for BudgetRange {
+    type Output = BudgetRange;
+
+    fn sub(self, other: Discount) -> BudgetRange {
+        BudgetRange {
+            low: self.low - other.0,
+            high: self.high - other.0,
+        }
+    }
+}
+
 fn main() {
     assert_eq!(
         BudgetRange { low: 10, high: 100 } + BudgetRange { low: 5, high: 50 },
         BudgetRange { low: 15, high: 150 }
-    )
+    );
+
+    assert_eq!(
+        BudgetRange { low: 10, high: 100 } - Discount(10),
+        BudgetRange { low: 0, high: 90 }
+    );
 }
