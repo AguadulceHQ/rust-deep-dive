93c822de0a18cd27705ddcb7fa2c9ec2b6bfc79e
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2023-01-19 22:00:18 +0400

    feat: add type name for long type names

--- type-aliases/src/main.rs
+++ type-aliases/src/main.rs
@@ -1,3 +1,6 @@
+// type aliases are useful for long type names
+type Thunk = Box<dyn Fn() + Send + 'static>;
+
 fn main() {
     // NPS is now synonm with u8
     // it's not a separate/new type it has same API
@@ -7,4 +10,19 @@ fn main() {
 
     // they are the same type so we can do usual operations
     println!("The final score is {}", score + nps_score);
+
+    // thunk means code to be evaluated at a later time
+    // proper type name for a closure that gets sorted
+    let mut f: Thunk = Box::new(|| println!("Hello"));
+
+    does_nothing_with_long_type(f);
+
+    f = does_nothing_and_returns_long_type();
+}
+
+fn does_nothing_with_long_type(_f: Thunk) {}
+
+fn does_nothing_and_returns_long_type() -> Thunk {
+    let f: Thunk = Box::new((|| println!("World")));
+    return f;
 }
