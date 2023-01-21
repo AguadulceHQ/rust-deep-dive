ccd2d996060d87a8dacccd9858849c46ec39b249
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2023-01-21 22:55:23 +0400

    feat: add dynamic traits with Sized and ?Sized traits

--- trait-dynamic-sized/src/main.rs
+++ trait-dynamic-sized/src/main.rs
@@ -10,4 +10,32 @@ fn main() {
     let s2: &str = "Aguadulce";
 
     println!("{} {}", s1, s2);
+
+    let integers = vec![3, 2, 42, 3];
+    println!("The max of the integers is {}", maximum(&integers));
+
+    println!(
+        "I am using a Sized trait implemented by default with give_back fn {}",
+        integers[0]
+    );
+}
+
+// this is actually equivalent to <T: Sized> because the compiler automatically adds a bound on Sized on any generic function
+fn give_back<T>(t: T) -> T {
+    t
+}
+
+// if we don't know if T will be known at compile time
+// fn generic_unsure<T: ?Sized>(t: &T) {}
+// this is equivalent to <T: ?Sized, std::cmp etc.>
+fn maximum<T: std::cmp::PartialOrd>(numbers: &[T]) -> &T {
+    let mut max = &numbers[0];
+
+    for number in numbers {
+        if number > max {
+            max = number;
+        }
+    }
+
+    max
 }
