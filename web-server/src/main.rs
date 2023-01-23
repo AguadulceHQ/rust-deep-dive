0936adfa83efda1d4c4cda4aa0e2aa7de29de7c3
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2023-01-23 21:58:00 +0400

    feat: add read request handler to web server

--- web-server/src/main.rs
+++ web-server/src/main.rs
@@ -1,4 +1,7 @@
-use std::net::TcpListener;
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     // 7878 is Rust typed on a phone lol
@@ -12,6 +15,28 @@ fn main() {
         let stream = stream.unwrap();
 
         // this prints in the terminal althoguh the page shows connection reset as we are not writing data
-        println!("Blazingly fast web server folks 🦀");
+        // println!("Blazingly fast web server folks 🦀");
+
+        connection_handler(stream);
     }
+}
+
+fn connection_handler(mut stream: TcpStream) {
+    // wraps a mutable reference to the stream into a BufReader instance
+    // BufReader adds buffering by managing calls to std::io::Read trait's methods for us
+    let buf_reader = BufReader::new(&mut stream);
+    // collect the lines of the request
+    // we collect those lines in a vector by using Vec<_> type annotation
+    let http_request: Vec<_> = buf_reader
+        // returns an iterator of Result<String, std::io::Error> and it splits the stream of data when it sees a newline
+        .lines()
+        // we map and unwrap each Result to get the actual string
+        // we stop the program if we have some invalid data
+        .map(|result| result.unwrap())
+        // the browser signals the end of an HTTP request by sending two newline chars in a row
+        // so we take lines until there is an empty string
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request received: {:#?}", http_request);
 }
