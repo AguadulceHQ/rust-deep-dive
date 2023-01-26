b4c131b369092957253831b835c4b370fe663bda
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2023-01-26 21:49:32 +0400

    feat: add new handler that validates incoming requests and throws 404 for invalid ones

--- web-server/src/main.rs
+++ web-server/src/main.rs
@@ -18,7 +18,7 @@ fn main() {
         // this prints in the terminal althoguh the page shows connection reset as we are not writing data
         // println!("Blazingly fast web server folks 🦀");
 
-        connection_handler(stream);
+        connection_handler_with_validation(stream);
     }
 }
 
@@ -51,4 +51,33 @@ fn connection_handler(mut stream: TcpStream) {
     let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{page}");
 
     stream.write_all(response.as_bytes()).unwrap();
+}
+
+fn connection_handler_with_validation(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    // we read just the first line of HTTP request
+    // so that we don't read the entire request into a vector
+    // we use next() to fetch the first item of the iteartor
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    // check if the client requested the / path
+    if request_line == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK";
+        let page = fs::read_to_string("hello.html").unwrap();
+        let length = page.len();
+
+        // format macro to add file's contents as the body of the success response
+        // to create a valid HTTP response we add the header Content-Length
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{page}");
+
+        stream.write_all(response.as_bytes()).unwrap();
+    } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND";
+        let page = fs::read_to_string("404.html").unwrap();
+        let length = page.len();
+
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{page}");
+
+        stream.write_all(response.as_bytes()).unwrap();
+    }
 }
