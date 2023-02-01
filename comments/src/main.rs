2cdbc73f827629c01db78411053543b96e80e9e1
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2023-02-01 21:10:56 +0400

    feat: add multiline comment

--- comments/src/main.rs
+++ comments/src/main.rs
@@ -5,4 +5,16 @@ fn main() {
     let x = 42;
 
     println!("Can you explain me what this number is about? {} 🤯", x);
+
+    add_twice(x);
+}
+
+/*
+    This is a comment that spans across multiple line
+    add_twice adds a number to itself
+    We will discover later a beter way to document functions
+*/
+
+fn add_twice(n: i32) -> i32 {
+    n + n
 }
