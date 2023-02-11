efc78730b076f1d66ece91ed3c60f0cdb173ee4e
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2023-02-11 23:29:14 +0400

    feat: add `C` like `enums` demonstration

--- examples/enum.rs
+++ examples/enum.rs
@@ -9,6 +9,20 @@ enum WebEvent {
     Click { x: i64, y: i64 },
 }
 
+fn inspect(event: WebEvent) {
+    match event {
+        WebEvent::PageLoad => println!("Page loaded"),
+        WebEvent::PageUnload => println!("Page unloaded"),
+        // destructure c from inside the enum
+        WebEvent::KeyPress(c) => println!("Pressed {}", c),
+        WebEvent::Paste(s) => println!("Pasted {}", s),
+        // destructure Click into x and y
+        WebEvent::Click { x, y } => {
+            println!("Clicked at x={}, y={}", x, y);
+        }
+    }
+}
+
 // type alias are useful if an enum is too long or generic and we want to rename it
 
 enum VeryLongNameThatWeWontType {
@@ -30,18 +44,20 @@ impl Operations {
     }
 }
 
-fn inspect(event: WebEvent) {
-    match event {
-        WebEvent::PageLoad => println!("Page loaded"),
-        WebEvent::PageUnload => println!("Page unloaded"),
-        // destructure c from inside the enum
-        WebEvent::KeyPress(c) => println!("Pressed {}", c),
-        WebEvent::Paste(s) => println!("Pasted {}", s),
-        // destructure Click into x and y
-        WebEvent::Click { x, y } => {
-            println!("Clicked at x={}, y={}", x, y);
-        }
-    }
+// two C like enums
+
+// implicit discriminator for this enum starts at 0
+enum Number {
+    Zero,
+    One,
+    Two,
+}
+
+// enum with explicit discriminator
+enum Color {
+    Red = 0xff0000,
+    Green = 0x00ff00,
+    Blue = 0x0000ff,
 }
 
 fn main() {
@@ -61,4 +77,8 @@ fn main() {
     // we can refer to each variant with its alias
     let x = Operations::Add;
     println!("2 plus 3 is {}", x.run(2, 3));
+
+    // enums can be cast as integers
+    println!("zero is {}", Number::Zero as i32); // this would cast to 0 also if it was say "Bla"
+    println!("roses are #{:06x}", Color::Red as i32);
 }
