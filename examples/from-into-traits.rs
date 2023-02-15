dab409c9bb4e41e506e03749dcd1f97fdbdd12cb
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2023-02-15 21:33:17 +0400

    feat: add `Into` trait example

--- examples/from-into-traits.rs
+++ examples/from-into-traits.rs
@@ -2,6 +2,7 @@
 // if you are able to convert type A from type B then it should be easy to go from B to A
 
 // From allows a type to define how to create itself from another type
+// Into is reciprocal to From if you implement the From trait for your type, Into calls it when neessary
 
 use std::convert::From;
 
@@ -20,5 +21,10 @@ impl From<i32> for Number {
 fn main() {
     // convert a u32 into a Number through From trait
     let num = Number::from(30);
+    println!("My number is {:?}", num);
+
+    let int = 42;
+    // because From is implemented on Number this is allowed
+    let num: Number = int.into();
     println!("My number is {:?}", num);
 }
