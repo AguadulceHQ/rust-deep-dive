ce7b0cd9c125d705a69d51479a1e50688a60522a
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2023-02-22 21:29:32 +0400

    feat: destructure a `enum` with if let

--- examples/if-let.rs
+++ examples/if-let.rs
@@ -30,4 +30,38 @@ fn main() {
     } else {
         println!("Didn't match a number and not even the other condition");
     }
+
+    // we can also destructure enums
+
+    // Our example enum
+    enum Foo {
+        Bar,
+        Baz,
+        Qux(u32),
+    }
+
+    enum Bar {
+        Foo,
+    }
+
+    let c = Foo::Qux(100);
+
+    // Variable c matches Foo::Qux which has a value
+    if let Foo::Qux(value) = c {
+        println!("c is {}", value);
+    }
+
+    // Binding also works with `if let`
+    if let Foo::Qux(value @ 100) = c {
+        println!("c is one hundred");
+    }
+
+    // now a more complex example
+    // we can match non parameterized enum variants that don't implement/derive PartialEq
+    let a = Bar::Foo;
+
+    // here if Foo::Bar == a would fail because PartialEq trait is not implemented
+    if let Bar::Foo = a {
+        println!("Matched a Barfoo");
+    }
 }
