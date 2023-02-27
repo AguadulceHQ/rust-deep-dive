f877dd32a7046b157bdeb4e57ed17a0bea4ba153
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2023-02-27 21:21:41 +0400

    feat: closure return as output from fn with move

---
+++ examples/closures-as-output.rs
@@ -0,0 +1,33 @@
+// we can also return closures
+// because closures are anonymous type we need to use the impl Trait to return them
+// valid traits are of cours Fn, FnMut, FnOnce
+
+// this needs to be clubbed with move because otherwise the capture will happen by reference and would be dropped as soon as the fn exited leaving an invalid reference in the closure
+
+fn create_fn() -> impl Fn() {
+    let text = "Fn".to_owned();
+
+    move || println!("This is a: {}", text)
+}
+
+fn create_fnmut() -> impl FnMut() {
+    let text = "FnMut".to_owned();
+
+    move || println!("This is a: {}", text)
+}
+
+fn create_fnonce() -> impl FnOnce() {
+    let text = "FnMut".to_owned();
+
+    move || println!("This is a: {}", text)
+}
+
+fn main() {
+    let fn_plain = create_fn();
+    let mut fn_mut = create_fnmut();
+    let fn_once = create_fnonce();
+
+    fn_plain();
+    fn_mut();
+    fn_once();
+}
