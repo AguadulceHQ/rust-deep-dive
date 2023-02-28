06772de21d474740fd488a52e741c47d02b5574d
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2023-02-28 21:35:52 +0400

    feat: comparsion between imperative vs functional programming with hihg order components

---
+++ examples/high-order-function.rs
@@ -0,0 +1,37 @@
+// HOF are fn that take one or more fn and or produce a more useful fn
+// HOF and lazy itertor give Rust its functional aspect
+
+fn is_odd(n: u32) -> bool {
+    n % 2 == 1
+}
+
+fn main() {
+    println!("Find the sum of all the squared numbers under 1000");
+    let upper = 1000;
+
+    // imperative approach
+    // declare accumulator variable
+    let mut acc = 0;
+    // iterate 0, 1, 2..infinity
+    for n in 0.. {
+        // square num
+        let n_squared = n * n;
+
+        if n_squared >= upper {
+            // break loop
+            break;
+        } else if is_odd(n_squared) {
+            // accumulate value if it's odd
+            acc += n_squared;
+        }
+    }
+    println!("Imperative style: {}", acc);
+
+    // functional approach
+    let sum_of_squared_odd_numers: u32 = (0..)
+        .map(|n| n * n)
+        .take_while(|&n_squared| n_squared < upper)
+        .filter(|&n_squared| is_odd(n_squared))
+        .sum();
+    println!("Functional style: {}", sum_of_squared_odd_numers);
+}
