4e24484c7a829040ae9397a7187d47db2c558234
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2023-03-01 21:26:15 +0400

    feat: nested module and visibility examples

--- examples/modules-visibility.rs
+++ examples/modules-visibility.rs
@@ -18,6 +18,59 @@ mod my_custom {
         print!("Called `my_custom::indirect_call()`, that\n> ");
         private_function();
     }
+
+    pub fn call_public_function_in_my_mod() {
+        print!("called `my_custom::call_public_function_in_my_mod()`, that\n> ");
+        nested::public_function_in_my_mod();
+        print!("> ");
+        nested::public_function_in_super_mod();
+    }
+
+    // pub(crate) makes functions visible only within the current crate
+    pub(crate) fn public_function_in_crate() {
+        println!("called `my_custom::public_function_in_crate()`");
+    }
+
+    pub mod nested {
+        pub fn function() {
+            println!("Called `my_custom::nested::function()`");
+        }
+
+        #[allow(dead_code)]
+        fn private_function() {
+            println!("Called `my_custom::nested::private_function()`");
+        }
+
+        // functions decleard using pub(in path) syntax are only visible within the given path.
+        // the path must be a parent or ancestor module
+        pub(in crate::my_custom) fn public_function_in_my_mod() {
+            print!("Called `my_custom::nested::public_function_in_my_mod()`, that\n> ");
+            public_function_in_nested();
+        }
+
+        // fn declared using pub(self) are only visible within the current module which is equivalent to leaving them private
+        pub(self) fn public_function_in_nested() {
+            println!("Called `my_custom::nested::public_function_in_nested()`");
+        }
+
+        // fn declared using pub(super) are visible within the parent module
+        pub(super) fn public_function_in_super_mod() {
+            println!("Called `my_custom::nested::public_function_in_super_mod()`");
+        }
+    }
+
+    mod private_nested {
+        #[allow(dead_code)]
+        pub fn function() {
+            println!("Called `my_custom::private_nested::function()`");
+        }
+
+        // private parent items will still restrict the visibility of a child item even if it is declared as visible within a bigger scope
+        #[allow(dead_code)]
+        pub(crate) fn restricted_function() {
+            println!("Called `my_custom::private_nested::restricted_function()`");
+        }
+    }
 }
 
 fn function() {
@@ -31,4 +84,10 @@ fn main() {
 
     // public items can be accessed
     my_custom::indirect_call();
+
+    // we can access this because the nested module is public and this item is public
+    my_custom::nested::function();
+
+    // pub(crate) items can be called from anywhere in the same crate
+    my_custom::public_function_in_crate();
 }
