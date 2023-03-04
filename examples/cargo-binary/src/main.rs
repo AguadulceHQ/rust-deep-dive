a6786402f73cc1aa58a0032b5c3da199c60f1cf1
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2023-03-04 23:11:58 +0400

    feat(crates): add default cargo usage to create new crates

---
+++ examples/cargo-binary/src/main.rs
@@ -0,0 +1,12 @@
+// cargo can manage depedencies for a project
+// it has also useful commands
+
+// A binary
+// cargo new foo
+
+// A library
+//cargo new --lib bar
+
+fn main() {
+    println!("Hello, world!");
+}
