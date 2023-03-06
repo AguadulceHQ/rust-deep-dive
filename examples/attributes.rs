1800afd4c59719b7fd9130ff8832e472a7632163
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2023-03-06 22:20:16 +0400

    feat: add attributes example

---
+++ examples/attributes.rs
@@ -0,0 +1,20 @@
+// metadata applied to some module, crate or item
+// generally used for conditional compilation of code, set crate name, disable lints, enable compiler features etc.
+// mark fn as unit tests, or part of a benchmark
+
+// #![crate_wide_attribute]
+
+// #[attribute(value)]
+
+#[derive(Debug)]
+struct Project {
+    id: u32,
+}
+
+fn main() {
+    let p = Project { id: 3 };
+    println!("{:?}", p);
+}
+
+// attributes can take multiple values
+// #[attribute(value, value2)]
