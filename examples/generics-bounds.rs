47ce9820ffe61ec909d87aa843d0532a076ae698
Luca Yesupatham Daniel <lucaydaniel@gmail.com>
2023-03-11 22:41:52 +0100

    feat(generics): trait bound on fn for generic type that needs to implement a specific trait

--- examples/generics-bounds.rs
+++ examples/generics-bounds.rs
@@ -7,6 +7,50 @@ fn printer<T: Display>(t: T) {
     println!("{}", t);
 }
 
+// you can also specify on a struct level
+struct S<T: Display>(T);
+
+// a trait that implements the print market {:?}
+use std::fmt::Debug;
+
+trait HasArea {
+    fn area(&self) -> f64;
+}
+
+impl HasArea for Rectangle {
+    fn area(&self) -> f64 {
+        self.length * self.height
+    }
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    length: f64,
+    height: f64,
+}
+#[allow(dead_code)]
+struct Triangle {
+    length: f64,
+    height: f64,
+}
+
+// the generic T must implement Debug, regardless of the specific type the fn should work if that's true
+fn print_debug<T: Debug>(t: &T) {
+    println!("{:?}", t);
+}
+
+// T must implement HasArea
+fn area<T: HasArea>(t: &T) -> f64 {
+    t.area()
+}
+
 fn main() {
     printer("Hello, World");
+
+    let rectangle = Rectangle {
+        length: 3.0,
+        height: 4.0,
+    };
+    print_debug(&rectangle);
+    println!("Area: {}", rectangle.area());
 }
